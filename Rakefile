81d0ab6932a4f4fae10c5397c24fd7c04f0c4a56
Andrew Peterson <andy@ndpsoftware.com>
2013-08-04 20:55:30 -0700

    remove some extra code; minor cleanup

--- Rakefile
+++ Rakefile
@@ -1,11 +1,8 @@
-require 'rubygems/package_task'
 require 'rspec/core/rake_task'
 
 include Rake::DSL
-GEM_ROOT = File.expand_path(File.dirname(__FILE__))
-$: << File.join(GEM_ROOT,'lib')
 
-ENV["RAILS_ENV"] ||= 'test'
+$: << File.join(File.expand_path(File.dirname(__FILE__)),'lib')
 
 RSpec::Core::RakeTask.new(:spec)
 task :default => :spec
--- spec/controller/save_the_campaign_spec.rb
+++ spec/controller/save_the_campaign_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
+describe StubController, type: 'controller' do
 
-
-describe StubController, type: :controller do
   context 'no configuration' do
     it 'redirects normally' do
       get 'foo'
@@ -10,36 +9,36 @@ describe StubController, type: :controller do
     end
   end
 
-  context 'initialized' do
-
-    before do
-      SaveTheCampaign.install
-    end
+  context 'installed' do
+    before { SaveTheCampaign.install }
 
     it 'preserves utm parameters' do
       campaign_params = {utm_source: 'the source', utm_campaign: 'the campaign', utm_medium: 'the medium'}
+
       get :foo, campaign_params
+
       response.should redirect_to campaign_params.merge(controller: 'stub', action: 'bar')
+    end
+
+    context 'with param overrides' do
+      before do
+        SaveTheCampaign.configure do |config|
+          config.params << 'email'
+          config.params << :name
+        end
+      end
+      it 'preserves parameter specified with a string' do
+        campaign_params = {utm_source: 'the source', email: 'a@example.com'}
+        get :foo, campaign_params
+        response.should redirect_to campaign_params.merge(controller: 'stub', action: 'bar')
+      end
+      it 'preserves parameters specified with symbols' do
+        campaign_params = {utm_source: 'the source', name: 'Joe Bob'}
+        get :foo, campaign_params
+        response.should redirect_to campaign_params.merge(controller: 'stub', action: 'bar')
+      end
     end
   end
 
-  context 'with param overrides' do
-    before do
-      SaveTheCampaign.configure do |config|
-        config.params << 'email'
-        config.params << :name
-      end
-    end
-    it 'preserves new parameters' do
-      campaign_params = {utm_source: 'the source', email: 'a@example.com'}
-      get :foo, campaign_params
-      response.should redirect_to campaign_params.merge(controller: 'stub', action: 'bar')
-    end
-    it 'preserves parameters specified with symbols' do
-      campaign_params = {utm_source: 'the source', name: 'Joe Bob'}
-      get :foo, campaign_params
-      response.should redirect_to campaign_params.merge(controller: 'stub', action: 'bar')
-    end
-  end
 end
 
